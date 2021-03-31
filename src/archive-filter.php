9a147a32afe0cb21c1e3665fc0549f491ed2d085
takty <tokutadaginaya@gmail.com>
2021-03-31 23:54:28 +0900

    Implementing

--- src/archive-filter.php
+++ src/archive-filter.php
@@ -40,7 +40,7 @@ function the_yearly_archive_select( array $args = array() ) {
 
 function get_custom_archives( $meta_key, $args = array() ) {
 	global $wpdb, $wp_locale;
-	$r = array_merge(
+	$args = array_merge(
 		array(
 			'type'            => 'monthly',
 			'limit'           => '',
@@ -55,80 +55,95 @@ function get_custom_archives( $meta_key, $args = array() ) {
 		$args
 	);
 
-	$post_type_object = get_post_type_object( $r['post_type'] );
+	$post_type_object = get_post_type_object( $args['post_type'] );
 	if ( ! is_post_type_viewable( $post_type_object ) ) {
 		return;
 	}
-	if ( ! empty( $r['limit'] ) ) {
-		$r['limit'] = absint( $r['limit'] );
-		$r['limit'] = ' LIMIT ' . $r['limit'];
+
+	$args['post_type'] = $post_type_object->name;
+
+	if ( '' === $args['type'] ) {
+		$args['type'] = 'monthly';
 	}
 
-	$order = strtoupper( $r['order'] );
+	if ( ! empty( $args['limit'] ) ) {
+		$args['limit'] = absint( $args['limit'] );
+		$args['limit'] = ' LIMIT ' . $args['limit'];
+	}
+
+	$order = strtoupper( $args['order'] );
 	if ( 'ASC' !== $order ) {
 		$order = 'DESC';
 	}
-	$where = $wpdb->prepare( "WHERE post_type = %s AND post_status = 'publish'", $r['post_type'] );
-	$where = apply_filters( 'getarchives_where', $where, $r );
+
+	$where = $wpdb->prepare( "WHERE post_type = %s AND post_status = 'publish'", $args['post_type'] );
+	$where = apply_filters( 'getarchives_where', $where, $args );
 	$join  = "INNER JOIN $wpdb->postmeta ON ( $wpdb->posts.ID = $wpdb->postmeta.post_id AND $wpdb->postmeta.meta_key = '$meta_key' )";
-	$join  = apply_filters( 'getarchives_join', $join, $r );
+	$join  = apply_filters( 'getarchives_join', $join, $args );
 
 	$output       = '';
 	$last_changed = wp_cache_get_last_changed( 'posts' );
-	$limit        = $r['limit'];
+	$limit        = $args['limit'];
 
-	if ( 'monthly' === $r['type'] ) {
-		$query = "SELECT YEAR(meta_value) AS `year`, MONTH(meta_value) AS `month`, count(ID) as posts FROM $wpdb->posts $join $where GROUP BY YEAR(meta_value), MONTH(meta_value) ORDER BY meta_value $order $limit";
-		$key   = md5( $query );
-		$key   = "wp_get_archives:$key:$last_changed";
-
+	if ( 'monthly' === $args['type'] ) {
+		$query   = "SELECT YEAR(meta_value) AS `year`, MONTH(meta_value) AS `month`, count(ID) as posts FROM $wpdb->posts $join $where GROUP BY YEAR(meta_value), MONTH(meta_value) ORDER BY meta_value $order $limit";
+		$key     = md5( $query );
+		$key     = "wp_get_archives:$key:$last_changed";
 		$results = wp_cache_get( $key, 'posts' );
 		if ( ! $results ) {
+			// phpcs:disable
 			$results = $wpdb->get_results( $query );
+			// phpcs:enable
 			wp_cache_set( $key, $results, 'posts' );
 		}
 		if ( $results ) {
-			$after = $r['after'];
+			$after = $args['after'];
 			foreach ( (array) $results as $result ) {
 				$url = get_month_link( $result->year, $result->month );
-				if ( 'post' !== $r['post_type'] ) {
-					$url = add_query_arg( 'post_type', $r['post_type'], $url );
+				if ( 'post' !== $args['post_type'] ) {
+					$url = add_query_arg( 'post_type', $args['post_type'], $url );
 				}
 				/* translators: 1: month name, 2: 4-digit year */
 				$text = sprintf( __( '%1$s %2$d' ), $wp_locale->get_month( $result->month ), $result->year );
-				if ( $r['show_post_count'] ) {
-					$r['after'] = '&nbsp;(' . $result->posts . ')' . $after;
+				if ( $args['show_post_count'] ) {
+					$args['after'] = '&nbsp;(' . $result->posts . ')' . $after;
 				}
-				$output .= get_archives_link( $url, $text, $r['format'], $r['before'], $r['after'] );
+				$selected = is_archive() && (string) $parsed_args['year'] === $result->year && (string) $parsed_args['monthnum'] === $result->month;
+				$output  .= get_archives_link( $url, $text, $args['format'], $args['before'], $args['after'], $selected );
 			}
 		}
-	} elseif ( 'yearly' === $r['type'] ) {
+	} elseif ( 'yearly' === $args['type'] ) {
 		$query = "SELECT YEAR(meta_value) AS `year`, count(ID) as posts FROM $wpdb->posts $join $where GROUP BY YEAR(meta_value) ORDER BY meta_value $order $limit";
 		$key   = md5( $query );
 		$key   = "wp_get_archives:$key:$last_changed";
 
 		$results = wp_cache_get( $key, 'posts' );
 		if ( ! $results ) {
+			// phpcs:disable
 			$results = $wpdb->get_results( $query );
+			// phpcs:enable
 			wp_cache_set( $key, $results, 'posts' );
 		}
 		if ( $results ) {
-			$after = $r['after'];
+			$after = $args['after'];
 			foreach ( (array) $results as $result ) {
 				$url = get_year_link( $result->year );
-				if ( 'post' !== $r['post_type'] ) {
-					$url = add_query_arg( 'post_type', $r['post_type'], $url );
+				if ( 'post' !== $args['post_type'] ) {
+					$url = add_query_arg( 'post_type', $args['post_type'], $url );
 				}
 				$text = sprintf( '%d', $result->year );
-				if ( $r['show_post_count'] ) {
-					$r['after'] = '&nbsp;(' . $result->posts . ')' . $after;
+				if ( $args['show_post_count'] ) {
+					$args['after'] = '&nbsp;(' . $result->posts . ')' . $after;
 				}
-				$output .= get_archives_link( $url, $text, $r['format'], $r['before'], $r['after'] );
+				$selected = is_archive() && (string) $parsed_args['year'] === $result->year;
+				$output  .= get_archives_link( $url, $text, $args['format'], $args['before'], $args['after'], $selected );
 			}
 		}
 	}
-	if ( $r['echo'] ) {
+	if ( $args['echo'] ) {
+		// phpcs:disable
 		echo $output;
+		// phpcs:enable
 	} else {
 		return $output;
 	}
@@ -158,21 +173,68 @@ function the_taxonomy_archive_select( array $args = array() ) {
 }
 
 function the_taxonomy_archive_option( $args = array() ) {
-	$args  = array_merge(
+	$args = array_merge(
 		array(
 			'hide_empty' => false,
 			'parent'     => 0,
 		),
 		$args
 	);
+
+	$output = '';
 	foreach ( get_terms( $taxonomy, $args ) as $t ) {
-		$link = get_term_link( $t );
-		echo '<option value="' . esc_attr( $link ) . '">' . esc_html( $t->name ) . '</option>' . "\n";
+		$url     = get_term_link( $t );
+		$output .= get_archives_link( $url, $t->name, 'option' );
+
 		$args['parent'] = $t->term_id;
 
 		foreach ( get_terms( $taxonomy, $args ) as $ct ) {
-			$link = get_term_link( $ct );
-			echo '<option value="' . esc_attr( $link ) . '">— ' . esc_html( $ct->name ) . '</option>' . "\n";
+			$url     = get_term_link( $ct );
+			$output .= get_archives_link( $url, '— ' . $ct->name, 'option' );
 		}
 	}
+	// phpcs:disable
+	echo $output;
+	// phpcs:enable
+}
+
+
+// -----------------------------------------------------------------------------
+
+
+/**
+ * The callback function for 'get_archive_link' filter.
+ *
+ * @param string $link_html The archive HTML link content.
+ * @param string $url       URL to archive.
+ * @param string $text      Archive text description.
+ * @param string $format    Link format. Can be 'link', 'option', 'html', or custom.
+ * @param string $before    Content to prepend to the description.
+ * @param string $after     Content to append to the description.
+ * @param bool   $selected  True if the current page is the selected archive.
+ * @return string The filtered link.
+ */
+function _cb_get_archives_link( string $link_html, string $url, string $text, string $format, string $before, string $after, bool $selected ): string {
+	if ( $selected ) {
+		if ( 'html' === $format ) {
+			$link_html = str_replace( "\t<li>", "\t<li class=\"current\">", $link_html );
+		}
+	}
+	static $urls = array();
+	if ( 'option' === $format ) {
+		if ( empty( $urls ) ) {
+			add_action(
+				'wp_footer',
+				function () use ( &$urls ) {
+					echo '<div style="display:none;"><!-- archive links of option values -->' . "\n";
+					foreach ( $urls as $url ) {
+						echo '<a href="' . esc_attr( $url ) . '"></a>' . "\n";
+					}
+					echo '</div>' . "\n";
+				}
+			);
+		}
+		$urls[] = $url;
+	}
+	return $link_html;
 }
